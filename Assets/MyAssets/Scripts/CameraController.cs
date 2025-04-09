7fea6629b29c81736221c5cc4ec462c7f3aeeff4
Ilia Riabko <ilia@Pechka02.>
2025-04-09 23:50:04 +0200

    Initial commit

---
+++ Assets/MyAssets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject MainCamera;
+
+    public void ShakeCamera()
+    {
+        MainCamera.transform.DOShakePosition(1.0f, 2f, 10, 90f);
+        Debug.Log("Shake");
+    }
+}
+
